06d5bb290bdb4628e5456d0ff7348efb81570651
R. Jacobs <rjacobs@retrorabbit.co.za>
2016-10-04 11:27:55 +0200

    Add initial user admin forms.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
+from .models import User, Profile
 
-# Register your models here.
+
+class ProfileInlineForm(admin.StackedInline):
+    model = Profile
+    max_num = 1
+    can_delete = False
+
+
+class UserProfileAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets
+    inlines = [ProfileInlineForm]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserProfileAdmin)
