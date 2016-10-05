11621613dce30737e41a04e93ec9b86c37b11ed2
R. Jacobs <rjacobs@retrorabbit.co.za>
2016-10-05 15:39:32 +0200

    Add basic content app forms.

---
+++ content/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+from .models import Challenge, Question, QuestionOption
+
+
+class ChallengeForm(forms.ModelForm):
+    class Meta:
+        model = Challenge
+        fields = ('name', 'activation_date', 'deactivation_date', 'state', 'end_processed')
+
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ('challenge', 'picture', 'text', 'type')
+
+
+class QuestionOptionForm(forms.ModelForm):
+    class Meta:
+        model = QuestionOption
+        fields = ('question', 'picture', 'text')
