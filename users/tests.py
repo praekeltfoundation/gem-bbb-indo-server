11df322100be267b9044c57b5b85ef7df3daf453
R. Jacobs <rjacobs@retrorabbit.co.za>
2016-10-06 15:08:39 +0200

    Add RegUser model test.

--- users/tests.py
+++ users/tests.py
@@ -3,12 +3,22 @@ from .models import User, RegUser, SysAdminUser, Profile
 
 
 class TestUserModel(TestCase):
-    def create_regular_user(self, first_name='Anon', last_name='Ymous', username='anonymous', password='Honkhonk'):
-        return RegUser.objects.create(first_name=first_name, last_name=last_name, username=username, password=password)
+    def create_regular_user(self, username='anonymous', **kwargs):
+        return RegUser.objects.create(username=username, **kwargs)
 
     def setUp(self):
         # TODO: Add test setup
         return None
 
+    def test_create_user(self):
+        u = User.objects.create_user(username='Anon', email='anon@ymous.org', password='Blarg')
+        self.assertIsNotNone(u, 'User not created.')
+        self.assertEqual(u.username, 'Anon', 'Username does not match')
+        p = Profile.objects.get(user=u)
+        self.assertIsNotNone(p, 'User profile not created.')
+
     def test_create_regular_user(self):
-        self.assertIsNotNone(self.create_regular_user())
+        u = self.create_regular_user(username='anonymous', password='Honkhonk')
+        self.assertIsNotNone(u, 'Regular user not created by proxy.')
+        self.assertFalse(u.is_staff, 'Regular user set as staff.')
+        self.assertFalse(u.is_superuser, 'Regular user set as superuser.')
