a3fae493eae10e0073669fe7e6f6b301eb1f1939
R. Jacobs <rjacobs@retrorabbit.co.za>
2016-10-06 14:44:29 +0200

    Add initial user model tests.

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import User, RegUser, SysAdminUser, Profile
 
-# Create your tests here.
+
+class TestUserModel(TestCase):
+    def create_regular_user(self, first_name='Anon', last_name='Ymous', username='anonymous', password='Honkhonk'):
+        return RegUser.objects.create(first_name=first_name, last_name=last_name, username=username, password=password)
+
+    def setUp(self):
+        # TODO: Add test setup
+        return None
+
+    def test_create_regular_user(self):
+        self.assertIsNotNone(self.create_regular_user())
