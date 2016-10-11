fed3b07f4da791c42f37473113004291a6be1777
Wimpie Victor <wvictor@retrorabbit.co.za>
2016-10-11 15:50:44 +0200

    Added tests for Tip REST resource (get).

--- content/tests.py
+++ content/tests.py
@@ -1,3 +1,89 @@
+import json
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from wagtail.wagtailcore.models import Page
+from users.models import User
+from .models import Tip
 
-# Create your tests here.
+
+def create_test_user():
+    """Creates a staff user."""
+    return User.objects.create(username='Anon', email='anon@ymous.org', password='Blarg',
+                               is_staff=True, is_superuser=False)
+
+
+def create_tip(title='Test tip', body='This is a test tip', **kwargs):
+    parent_page = Page.get_root_nodes()[0]
+    tip = Tip(
+        title=title,
+        body=body,
+        **kwargs,
+    )
+    parent_page.add_child(instance=tip)
+    tip.save()
+    tip.unpublish()
+    return tip
+
+
+def publish_page(user, page):
+    revision = page.save_revision(
+        user=user,
+        submitted_for_moderation=False,
+    )
+    revision.publish()
+
+
+class TestTipModel(TestCase):
+
+    def setUp(self):
+        self.user = create_test_user()
+
+    def tearDown(self):
+        self.user.delete()
+
+    def test_create_tip(self):
+        tip = create_tip()
+        self.assertIsNotNone(tip, 'Tip not created')
+        self.assertEqual(tip.title, 'Test tip', 'Test tip title was not set.')
+
+    def test_cover_image_url(self):
+        self.skipTest('Needs to instantiate a wagtail Image')
+        # TODO: Instatiate Image
+        # from django.core.files.images import ImageFile
+        # from wagtail.wagtailimages.models import Image
+        #
+        # image = Image(
+        #     title="Image title",
+        #
+        #     # image_file is your StringIO/BytesIO object
+        #     file=ImageFile(image_file, name="image-filename.jpg"),
+        # )
+        # image.save()
+
+    def test_cover_none(self):
+        """When a Tip does not have a cover image set, the image url must be None."""
+        tip = create_tip()
+        self.assertIsNone(tip.get_cover_image_url(), 'Cover image url is not None.')
+
+
+class TestTipAPI(APITestCase):
+
+    def setUp(self):
+        self.user = create_test_user()
+
+    def tearDown(self):
+        self.user.delete()
+
+    def test_draft_filter(self):
+        """When listing Tips in the REST endpoint, only published tips should be included."""
+        draft_tip = create_tip(title='Draft tip')
+        draft_tip.unpublish()
+        live_tip = create_tip(title='Live tip')
+        publish_page(self.user, live_tip)
+
+        response = self.client.get(reverse('tip-list'))
+        data = json.loads(response.content.decode('utf-8'))
+        self.assertEqual(Tip.objects.all().count(), 2, 'Test did not set up Tip pages correctly.')
+        self.assertEqual(len(data), 1, 'View returned more than one Tip.')
+        self.assertEqual(data[0]['title'], 'Live tip', 'The returned Tip was not the expected live page.')
