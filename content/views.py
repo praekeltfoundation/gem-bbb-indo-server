d27816a8ee96aaef913b823ca3cf1747f710a580
Wimpie Victor <wvictor@retrorabbit.co.za>
2016-10-12 11:52:41 +0200

    Added browsable API for Tips.

--- content/views.py
+++ content/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.renderers import JSONRenderer
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 from rest_framework import viewsets
 from rest_framework.response import Response
 from .models import Challenge, Tip
@@ -24,7 +24,7 @@ class ChallengeViewSet(viewsets.ModelViewSet):
 class TipViewSet(viewsets.ModelViewSet):
     queryset = Tip.objects.all()
     serializer_class = TipSerializer
-    renderer_classes = (JSONRenderer,)
+    renderer_classes = (JSONRenderer, BrowsableAPIRenderer)
 
     def list(self, request, *args, **kwargs):
         serializer = self.get_serializer(self.get_queryset().filter(live=True), many=True)
