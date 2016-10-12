0395f41a58bae0bb6afa752d002b6e62ea1b6f80
R. Jacobs <rjacobs@retrorabbit.co.za>
2016-10-12 14:55:10 +0200

    Add REST API for regular users.

--- bimbingbung/urls.py
+++ bimbingbung/urls.py
@@ -6,6 +6,7 @@ from django.contrib import admin
 
 from search import views as search_views
 from content import views as content_views
+from users import views as user_views
 from wagtail.wagtailadmin import urls as wagtailadmin_urls
 from wagtail.wagtailcore import urls as wagtail_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
@@ -23,6 +24,9 @@ urlpatterns = [
 
     url(r'^api/tips/$', content_views.TipViewSet.as_view({'get': 'list'}), name='tip-list'),
     url(r'^api/tips/(?P<pk>[0-9]+)/$', content_views.TipViewSet.as_view({'get': 'retrieve'}), name='tip-detail'),
+
+    url(r'^api/users/$', user_views.RegUserViewSet.as_view({'get': 'list', 'post': 'create'}), name='user-list'),
+    url(r'^api/users/(?P<pk>[0-9]+)/$', user_views.RegUserViewSet.as_view({'get': 'retrieve'}), name='user-detail'),
 
     url(r'', include(wagtail_urls)),
 ]
