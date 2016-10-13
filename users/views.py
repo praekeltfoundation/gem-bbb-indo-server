369d7d143531dc78a176ca392ac0aaeba1eddb4d
Wimpie Victor <wvictor@retrorabbit.co.za>
2016-10-13 14:25:55 +0200

    Clean up view comments.

--- users/views.py
+++ users/views.py
@@ -1,12 +1,9 @@
-from django.shortcuts import render
 
-# Create your views here.
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.response import Response
 from .models import RegUser
 from .serializers import RegUserDeepSerializer
-# Create your views here.
 
 
 class RegUserViewSet(viewsets.ModelViewSet):
