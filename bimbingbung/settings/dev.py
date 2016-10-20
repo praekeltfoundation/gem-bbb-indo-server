6a4be0ae0f6329d0e4f16ed8d2aeba4702c99aca
Wimpie Victor <wvictor@retrorabbit.co.za>
2016-10-20 16:34:37 +0200

    Added django-sendfile dependency.

--- bimbingbung/settings/dev.py
+++ bimbingbung/settings/dev.py
@@ -12,6 +12,13 @@ SECRET_KEY = 'eh+r*5&#=5a_w!ln_5yux2_d69v73q71n6j!=+x16^6z48p^w$'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
+# SENDFILE settings
+
+SENDFILE_BACKEND = 'sendfile.backends.development'
+SENDFILE_ROOT = os.path.join(PROJECT_DIR, 'protected')
+SENDFILE_URL = '/protected'
+
+
 try:
     from .local import *
 except ImportError:
