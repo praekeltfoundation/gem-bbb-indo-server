f5a81a87acc2906f912b266af520b26e750b1068
R. Jacobs <rjacobs@retrorabbit.co.za>
2016-11-14 11:39:19 +0200

    Add storage for challenge pictures.

--- content/storage.py
+++ content/storage.py
@@ -11,7 +11,16 @@ class GoalImgStorage(FileSystemStorage):
 class ParticipantPictureStorage(FileSystemStorage):
     def __init__(self, location=None, base_url=None, **kwargs):
         if location is None:
+            location = join(settings.SENDFILE_ROOT, 'participant_pics')
+        if location is None:
+            base_url = settings.SENDFILE_URL + 'participant_pics/'
+        super(ParticipantPictureStorage, self).__init__(location=location, base_url=base_url, **kwargs)
+
+
+class ChallengeStorage(FileSystemStorage):
+    def __init__(self, location=None, base_url=None, **kwargs):
+        if location is None:
             location = join(settings.SENDFILE_ROOT, 'challenge')
         if location is None:
             base_url = settings.SENDFILE_URL + 'challenge/'
-        super(ParticipantPictureStorage, self).__init__(location=location, base_url=base_url, **kwargs)
+        super(ChallengeStorage, self).__init__(location=location, base_url=base_url, **kwargs)
