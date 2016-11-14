e4c2d9fcd8fd325079cc1b3c8bcca8403ffa737c
R. Jacobs <rjacobs@retrorabbit.co.za>
2016-11-14 11:51:14 +0200

    Add image field to challenge model.

--- content/models.py
+++ content/models.py
@@ -1,7 +1,7 @@
-
 from collections import OrderedDict
 from datetime import timedelta
 from functools import reduce
+from os.path import splitext
 
 from django.contrib.auth.models import User
 from django.db import models
@@ -17,7 +17,11 @@ from wagtail.wagtailcore import models as wagtail_models
 from wagtail.wagtailimages import edit_handlers as wagtail_image_edit
 from wagtail.wagtailimages import models as wagtail_image_models
 
-from .storage import GoalImgStorage, ParticipantPictureStorage
+from .storage import ChallengeStorage, GoalImgStorage, ParticipantPictureStorage
+
+
+def get_challenge_image_filename(instance, filename):
+    return 'challenge-{}'.format(instance.pk, splitext(filename))
 
 
 @python_2_unicode_compatible
@@ -53,6 +57,11 @@ class Challenge(models.Model):
             (CTP_FREEFORM, _('Free text')),
         ),
         default=CTP_QUIZ)
+    picture = models.ImageField(_('picture'),
+                                upload_to=get_challenge_image_filename,
+                                storage=ChallengeStorage(),
+                                null=True,
+                                blank=True)
     end_processed = models.BooleanField(_('processed'), default=False)
 
     class Meta:
