a9682943993b1ec23963fbf11d2c74974d6389df
R. Jacobs <rjacobs@retrorabbit.co.za>
2016-11-17 10:17:18 +0200

    Manually specify static file namespace.

--- bimbingbung/urls.py
+++ bimbingbung/urls.py
@@ -43,8 +43,8 @@ urlpatterns = [
 
 if settings.DEBUG:
     from django.conf.urls.static import static
-    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    from django.contrib.staticfiles import views
 
     # Serve static and media files from development server
-    urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [url(r'^static/(?P<path>.*)$', views.serve, name='static')]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT, name='media')
