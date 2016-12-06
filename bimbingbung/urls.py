c3271ce7b8fa254a3fab6734a1edb2c4adc38911
R. Jacobs <rjacobs@retrorabbit.co.za>
2016-12-06 10:18:04 +0200

    Add feedback endpoint.

--- bimbingbung/urls.py
+++ bimbingbung/urls.py
@@ -17,12 +17,13 @@ from wagtail.wagtaildocs import urls as wagtaildocs_urls
 router = DefaultRouter()
 router.register(r'challenges', content_views.ChallengeViewSet, base_name='challenges')
 router.register(r'entries', content_views.EntryViewSet, base_name='entries')
+router.register(r'feedback', content_views.FeedbackViewSet, base_name='feedback')
+router.register(r'goals', content_views.GoalViewSet, base_name='goals')
 router.register(r'participantanswers', content_views.ParticipantAnswerViewSet, base_name='participantanswers')
 router.register(r'participantfreetext', content_views.ParticipantFreeTextViewSet, base_name='participantfreetext')
 router.register(r'participantpicture', content_views.ParticipantPictureViewSet, base_name='participantpicture')
 router.register(r'participants', content_views.ParticipantViewSet, base_name='participants')
 router.register(r'tips', content_views.TipViewSet, base_name='tips')
-router.register(r'goals', content_views.GoalViewSet, base_name='goals')
 router.register(r'users', user_views.RegUserViewSet, base_name='users')
 
 api_urls = [
