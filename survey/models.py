da0cb272f4da4a54f96d2fe196df2f56edcf05a5
Wimpie Victor <wvictor@retrorabbit.co.za>
2016-12-21 15:33:08 +0200

    Added delivery days

--- survey/models.py
+++ survey/models.py
@@ -1,28 +1,47 @@
 
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from wagtail.wagtailcore.models import Page
 from wagtail.wagtailadmin.edit_handlers import MultiFieldPanel, InlinePanel
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
 from modelcluster.fields import ParentalKey
 from wagtailsurveys.models import AbstractSurvey, AbstractFormField
 
 
-# =============== #
-# Dynamic Surveys #
-# =============== #
-
-
 class CoachSurvey(AbstractSurvey):
-    intro = models.TextField(help_text=_("The opening line said by the Coach when introducing the Survey."),
-                             blank=True, null=False)
-    outro = models.TextField(help_text=_("The closing line said by the Coach when finishing the Survey."),
-                             blank=True, null=False)
+    # Translators: Field name on CMS
+    intro = models.TextField(
+        verbose_name=_('intro dialogue'),
+        help_text=_("The opening line said by the Coach when introducing the Survey."),
+        blank=True, null=False
+    )
+
+    # Translators: Field name on CMS
+    outro = models.TextField(
+        verbose_name=_('outro dialogue'),
+        help_text=_("The closing line said by the Coach when finishing the Survey."),
+        blank=True, null=False
+    )
+
+    # Translators: Field name on CMS
+    deliver_after = models.PositiveIntegerField(
+        verbose_name=_('days to deliver'),
+        help_text=_("The number of days after user registration that the Survey will be available."),
+        default=1
+    )
 
     def get_form_fields(self):
         return self.custom_form_fields.all()
 
 
 CoachSurvey.content_panels = AbstractSurvey.content_panels + [
+    MultiFieldPanel(
+        [
+            FieldPanel('deliver_after')
+        ],
+        # Translators: Admin field name
+        heading=_('Survey')
+    ),
     MultiFieldPanel(
         [
             FieldPanel('intro'),
