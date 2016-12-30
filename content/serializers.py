cddd5dee55f053304e3db9e426ef5195938272fc
Wimpie Victor <wvictor@retrorabbit.co.za>
2016-12-30 14:19:38 +0200

    Changed prototype field to help Java code be more explicit

--- content/serializers.py
+++ content/serializers.py
@@ -500,7 +500,7 @@ class GoalTransactionSerializer(serializers.ModelSerializer):
 
 class GoalSerializer(serializers.ModelSerializer):
     name = serializers.CharField()
-    prototype = serializers.PrimaryKeyRelatedField(queryset=GoalPrototype.objects.all(), allow_null=True, required=False)
+    prototype_id = serializers.PrimaryKeyRelatedField(queryset=GoalPrototype.objects.all(), allow_null=True, required=False)
     start_date = serializers.DateField()
     end_date = serializers.DateField()
     value = serializers.ReadOnlyField()
