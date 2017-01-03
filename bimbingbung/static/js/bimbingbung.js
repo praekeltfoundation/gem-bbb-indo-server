85492d8d5991316ff7fb19f14f1f0aa51afc88bc
ReinhardtCromhout <RCromhout@retrorabbit.co.za>
2017-01-03 16:20:48 +0200

    The source of the image buttons change depending if they are on or off

--- bimbingbung/static/js/bimbingbung.js
+++ bimbingbung/static/js/bimbingbung.js
@@ -1,5 +1,7 @@
 var currIndex = 0;
 var list = document.getElementById("tip-nav-dots").getElementsByTagName("li");
+var leftButton = document.getElementById("left-nav-button");
+var rightButton = document.getElementById("right-nav-button");
 
 function empDot(int){
     if(int == -1){
@@ -18,5 +20,17 @@ function empDot(int){
             list[currIndex].setAttribute("id", "activated");
             scroll(0,0);
         }
+    }
+    if(currIndex == 0){
+        leftButton.src="/static/img/Tips Buttons_Previous_OFF.svg";
+    }
+    if(currIndex == 1){
+        leftButton.src="/static/img/Tips Buttons_Previous_ON.svg";
+    }
+    if(currIndex == (list.length-1)){
+        rightButton.src="/static/img/Tips Buttons_Next_OFF.svg";
+    }
+    if(currIndex == (list.length-2)){
+        rightButton.src="/static/img/Tips Buttons_Next_ON.svg";
     }
 }
