2c45e5193ae87aab1c8016f4731b90a3c1abb900
ReinhardtCromhout <RCromhout@retrorabbit.co.za>
2017-01-03 15:37:37 +0200

    Fixed the positioning of navigation buttons and navigation dots, issue #317 AND changed the app so that navigating across the tips can no longer wrap around from the last to the first tip or from the first to the last tip, issue #294

--- bimbingbung/static/js/bimbingbung.js
+++ bimbingbung/static/js/bimbingbung.js
@@ -1,16 +1,22 @@
 var currIndex = 0;
-var list = document.getElementById("tip-nav").getElementsByTagName("li");
+var list = document.getElementById("tip-nav-dots").getElementsByTagName("li");
 
 function empDot(int){
-    if(int === -1){
-        list[currIndex].removeAttribute("id");
-        currIndex = ((currIndex + list.length)-1) % list.length;
-        list[currIndex].setAttribute("id", "activated");
-        scroll(0,0);
+    if(int == -1){
+        if(currIndex != 0){
+            mySwipe.prev();
+            list[currIndex].removeAttribute("id");
+            currIndex = ((currIndex + list.length)-1) % list.length;
+            list[currIndex].setAttribute("id", "activated");
+            scroll(0,0);
+        }
     }else{
-        list[currIndex].removeAttribute("id");
-        currIndex = (currIndex+1) % list.length;
-        list[currIndex].setAttribute("id", "activated");
-        scroll(0,0);
+        if(currIndex != (list.length - 1)){
+            mySwipe.next();
+            list[currIndex].removeAttribute("id");
+            currIndex = (currIndex+1) % list.length;
+            list[currIndex].setAttribute("id", "activated");
+            scroll(0,0);
+        }
     }
 }
