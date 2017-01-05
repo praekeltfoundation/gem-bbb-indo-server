f5eedce7fdcfe7c4b2590fda68ded8501ccefd57
Wimpie Victor <wvictor@retrorabbit.co.za>
2017-01-05 08:44:39 +0200

    Added test boilerplate.

--- survey/tests.py
+++ survey/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from rest_framework.test import APITestCase
+
+from .models import CoachSurvey, CoachFormField
+
+
+def create_survey(title='Test Survey', intro='Take this challenge', outro='Thanks for taking the challenge',
+                  deliver_after=1):
+    return CoachSurvey.objects.create(
+        title=title,
+        intro=intro,
+        outro=outro,
+        deliver_after=deliver_after
+    )
+
+
+def publish(survey):
+    survey.save_revision(
+        user=creator,
+        submitted_for_moderation=False
+    ).publish()
+
+
+class CoachSurveyAPITest(APITestCase):
+
+    pass
