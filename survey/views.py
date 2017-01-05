a0097002e9fae91b57d22b800c7dfd80ed985003
Wimpie Victor <wvictor@retrorabbit.co.za>
2017-01-05 08:15:59 +0200

    Accepting survey submissions

--- survey/views.py
+++ survey/views.py
@@ -1,5 +1,6 @@
 
-from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.exceptions import ValidationError
 from rest_framework.decorators import detail_route
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -13,6 +14,7 @@ class CoachSurveyViewSet(ModelViewSet):
     queryset = CoachSurvey.objects.all()
     serializer_class = CoachSurveySerializer
     permission_classes = (IsAuthenticated,)
+    # post required for `submission`, but implicitly not allowed for survey itself
     http_method_names = ('head', 'options', 'get', 'post')
 
     def get_queryset(self):
@@ -21,4 +23,11 @@ class CoachSurveyViewSet(ModelViewSet):
 
     @detail_route(['post'])
     def submission(self, request, pk=None, *args, **kwargs):
-        return Response({})
+        survey = self.get_object()
+        # Leveraging form to validate fields
+        form = survey.get_form(request.POST, page=survey, user=request.user)
+        if form.is_valid():
+            survey.process_form_submission(form)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            raise ValidationError(form.errors)
