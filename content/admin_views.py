7b2563031b3aef111aeeb6f71e7d32c6f1bd9895
Chad Garrett <cgarrett@retrorabbit.co.za>
2017-01-24 15:19:03 +0200

    Admin participant interface

---
+++ content/admin_views.py
@@ -0,0 +1,25 @@
+
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import permission_required
+from django.http.response import JsonResponse
+
+from .models import Participant
+
+
+def participant_list_view(request):
+    context = {
+        'participants': Participant.objects.all()
+    }
+
+    return render(request, 'content/admin/participant/list.html', context)
+
+
+@permission_required('participant.can_change')
+@ensure_csrf_cookie
+def participant_mark_read(request, participant_pk):
+    participant = get_object_or_404(Participant, pk=participant_pk)
+
+    participant.save()
+    print('Participant ' + str(participant_pk))
+    return JsonResponse({})
