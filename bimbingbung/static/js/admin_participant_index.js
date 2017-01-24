a0b64a10b5b2895e5f562eb7cf14abb83ed1546c
Chad Garrett <cgarrett@retrorabbit.co.za>
2017-01-24 15:49:40 +0200

    Changes to use POST requests

--- bimbingbung/static/js/admin_participant_index.js
+++ bimbingbung/static/js/admin_participant_index.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
+        var csrftoken;
+        csrftoken = Cookies.get('csrftoken');
+
        $('.mark-is-read').each(function(index){
             $(this).change(function(){
                 /*console.log($(this).val() + ':' + this.checked);*/
                 $(".mark-is-read").prop("readonly", true);
                 $.ajax({
+                    beforeSend: function(xhr, settings) {
+                        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+                        }
+                    },
                     url: '/admin/participants/mark-read/' + $(this).val() + '/',
                     method: "POST"
                 });
@@ -11,3 +19,8 @@ $(document).ready(function() {
             });
        });
 });
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
