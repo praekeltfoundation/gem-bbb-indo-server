c6ff5381c7d3c43ac70071ef6bea9606eaa57906
ReinhardtCromhout <RCromhout@retrorabbit.co.za>
2017-01-27 21:00:54 +0200

    Final fix to migrations

--- content/migrations/0065_change_badge_setting_from_8_to_6.py
+++ content/migrations/0065_change_badge_setting_from_8_to_6.py
@@ -9,5 +9,9 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RenameField('badgesettings', 'weekly_target_8', 'weekly_target_6')
+        migrations.RenameField('badgesettings', 'weekly_target_8', 'weekly_target_6'),
+        migrations.AlterField(
+            model_name='badgesettings',
+            name='weekly_target_6',
+            field=models.ForeignKey(help_text='Awarded when a user has reached their weekly target 6 weeks in a row.', null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='+', to='content.Badge', verbose_name='6 Week On Target'))
         ]
