77ed6f7c42e07cb1921d46e18ceb0c4893712ee0
Chad Garrett <cgarrett@retrorabbit.co.za>
2017-02-01 08:09:53 +0200

    Added \d+ plus to include larger decimal places

--- content/admin_urls.py
+++ content/admin_urls.py
@@ -8,7 +8,7 @@ from .admin_views import participant_mark_winner
 
 
 urlpatterns = [
-    url(r'^mark-read/(?P<participant_pk>\d)/$', participant_mark_read, name='participant-mark-read'),
-    url(r'^mark-shortlisted/(?P<participant_pk>\d)/$', participant_mark_shortlisted, name='participant-mark-shortlisted'),
-    url(r'^mark-winner/(?P<participant_pk>\d)/$', participant_mark_winner, name='participant-mark-winner'),
+    url(r'^mark-read/(?P<participant_pk>\d+)/$', participant_mark_read, name='participant-mark-read'),
+    url(r'^mark-shortlisted/(?P<participant_pk>\d+)/$', participant_mark_shortlisted, name='participant-mark-shortlisted'),
+    url(r'^mark-winner/(?P<participant_pk>\d+)/$', participant_mark_winner, name='participant-mark-winner'),
 ]
