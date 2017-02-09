a73fa21d6281c97bb1e4ae84f894e7ee3b261424
Chad Garrett <cgarrett@retrorabbit.co.za>
2017-02-09 15:29:34 +0200

    Added user information export functionality

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
+from import_export import fields
+from import_export import resources
+from import_export.admin import ExportMixin
+
 from .models import User, Profile, SysAdminUser, RegUser
 
 
@@ -9,7 +13,59 @@ class ProfileInlineForm(admin.StackedInline):
     can_delete = False
 
 
-class UserProfileAdmin(UserAdmin):
+class UserResource(resources.ModelResource):
+    username = fields.Field()
+    full_name = fields.Field()
+    mobile = fields.Field()
+    email = fields.Field()
+    gender = fields.Field()
+    age = fields.Field()
+
+
+    class Meta:
+        model = Profile
+        fields = ('username', 'full_name', 'mobile', 'email', 'gender', 'age',)
+        export_order = ('username', 'full_name', 'mobile', 'email', 'gender', 'age',)
+
+    def dehydrate_username(self, user):
+        if user.username is not None:
+            return user.username
+        else:
+            return ""
+
+    def dehydrate_full_name(self, user):
+        if user.last_name is not None:
+            return user.get_full_name()
+        else:
+            return ""
+
+    def dehydrate_mobile(self, user):
+        if user.profile.mobile is not None:
+            return user.profile.mobile
+        else:
+            return ""
+
+    def dehydrate_email(self, user):
+        if user.email is not None:
+            return user.email
+        else:
+            return ""
+
+    def dehydrate_gender(self, user):
+        if user.profile.gender is not None:
+            return user.profile.gender
+        else:
+            return ""
+
+    def dehydrate_age(self, user):
+        if user.profile.age is not None:
+            return user.profile.age
+        else:
+            return ""
+
+
+class UserProfileAdmin(ExportMixin, UserAdmin):
+    resource_class = UserResource
     fieldsets = UserAdmin.fieldsets
     inlines = [ProfileInlineForm]
 
