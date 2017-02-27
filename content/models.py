b661aed516765eadc47a9b58d13540690bcc1465
Chad Garrett <cgarrett@retrorabbit.co.za>
2017-02-27 14:24:13 +0200

    Goal deadline notifications checks target first

--- content/models.py
+++ content/models.py
@@ -1260,6 +1260,11 @@ class Goal(models.Model):
 
         return streak
 
+    def is_goal_deadline_missed(self):
+        if timezone.now().date() > self.end_date and self.is_active and self.value < self.target:
+            return True
+        else:
+            return False
 
     def __str__(self):
         return self.name
@@ -1656,7 +1661,7 @@ class CustomNotification(models.Model):
     @classmethod
     def get_all_current_notifications(cls):
         """Returns all currently available custom notifications"""
-        notifications = CustomNotification.objects.filter(publish_date__lt=timezone.now(),
+        notifications = CustomNotification.objects.filter(publish_date__lte=timezone.now(),
                                                           expiration_date__gt=timezone.now())
 
         return notifications
