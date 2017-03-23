b564e027fc0912d792c9f2f464fdaf02c143092e
Chad Garrett <cgarrett@retrorabbit.co.za>
2017-03-23 12:35:24 +0200

    Downgraded celery version

--- content/tasks.py
+++ content/tasks.py
@@ -14,6 +14,7 @@ def just_print():
 
 @app.task(ignore_result=True, max_retries=10, default_retry_delay=10)
 def remove_report_archives():
+    print("Starting removal")
     for filename in os.listdir(settings.SENDFILE_ROOT):
         if filename.endswith('.csv') or filename.endswith('.zip'):
             print("Removing: " + settings.SENDFILE_ROOT + '\\' + filename)
