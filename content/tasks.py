e12d6e487982b8373d839006d3a0a5e0265d4ef9
Chad Garrett <cgarrett@retrorabbit.co.za>
2017-03-29 09:37:55 +0200

    Added celery and beat task to periodically query GA

--- content/tasks.py
+++ content/tasks.py
@@ -1,9 +1,21 @@
 # -*- coding: utf-8 -*-
 import os
+import argparse
+
+from apiclient.discovery import build
+from oauth2client.service_account import ServiceAccountCredentials
+import oauth2client.contrib
+import httplib2
+
+from oauth2client import client
+from oauth2client import file
+from oauth2client import tools
 
 from celery.task import task
+
 from django.conf import settings
 
+from content.analytics_api import get_report, print_response, initialize_analytics_reporting
 from content.celery import app
 
 
@@ -19,3 +31,12 @@ def remove_report_archives():
         if filename.endswith('.csv') or filename.endswith('.zip'):
             print("Removing: " + settings.SENDFILE_ROOT + '\\' + filename)
             os.remove(settings.SENDFILE_ROOT + '\\' + filename)
+
+
+@app.task(ignore_result=True, max_retries=10, default_retry_delay=10)
+def ga_task_handler():
+    print("Starting GA connection")
+    analytics = initialize_analytics_reporting()
+    response = get_report(analytics)
+    print_response(response)
+    print("Finished GA connection")
