858082d982e23729ec97c5a6622042a66045a0c9
Chad Garrett <cgarrett@retrorabbit.co.za>
2017-04-12 14:40:25 +0200

    Changed celery task to run hourly

--- bimbingbung/settings/base.py
+++ bimbingbung/settings/base.py
@@ -206,7 +206,7 @@ CELERYBEAT_SCHEDULE = {
     },
     'reconcile-ga-with-users': {
         'task': 'content.tasks.ga_task_handler',
-        'schedule': timedelta(minutes=15),
+        'schedule': timedelta(hours=1),
     },
 }
 
