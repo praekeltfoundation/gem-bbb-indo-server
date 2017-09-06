f558baf9dac740aa1c575b96da7815ce2e643b2c
Wimpie Victor <wvictor@retrorabbit.co.za>
2017-09-06 13:51:28 +0200

    Require staff status on mark user

--- survey/admin_views.py
+++ survey/admin_views.py
@@ -1,11 +1,12 @@
 
+from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import get_object_or_404
 from django.http.response import JsonResponse
 
 from .models import EndlineSurveySelectUser
 
 
-# TODO: permission_required('survey.can_change') Decide on proper permission
+@staff_member_required
 def survey_mark_can_receive(request, user_id):
     survey_accessor = get_object_or_404(EndlineSurveySelectUser, pk=user_id)
     survey_accessor.receive_survey = not survey_accessor.receive_survey
